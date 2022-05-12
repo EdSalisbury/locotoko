20d11bd29e02fac37a5e94a657c1cf658e373578
Ed Salisbury <ed.salisbury@gmail.com>
2022-05-12 16:21:05 -0600

    Added owner delete

--- client/src/components/OwnerList.vue
+++ client/src/components/OwnerList.vue
@@ -14,6 +14,26 @@
             {{ data.item.name }}
           </router-link>
         </template>
+
+        <template #cell(actions)="data">
+          <router-link
+            :to="'/editOwner/' + data.item.id"
+          >
+            <b-button
+              class="p-1 m-1"
+              variant="primary"
+            >
+              <b-icon-pencil-fill />
+            </b-button>
+          </router-link>
+          <b-button
+            class="p-1 m-1"
+            variant="danger"
+            @click="deleteOwner(data.item.id)"
+          >
+            <b-icon-trash-fill />
+          </b-button>
+        </template>
       </b-table>
       <router-link to="/addOwner"
         ><b-button variant="primary"
@@ -34,12 +54,35 @@ export default {
       fields: [
         { key: "ownerLink", label: "Name" },
         "rate",
+        "actions",
       ],
     };
   },
   async created() {
     const token = this.$cookie.get("token");
     this.owners = await api.getOwners(token);
+  },
+  methods: {
+    async deleteOwner(id) {
+      const url =
+        process.env.VUE_APP_API_BASE_URL +
+        "/api/v1/owners/" +
+        id;
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          Authorization:
+            "Bearer " + this.$cookie.get("token"),
+        },
+      });
+      if (response.status == 204) {
+        this.owners = this.owners.filter(
+          (owner) => owner.id !== id,
+        );
+      } else {
+        console.error(response);
+      }
+    },
   },
 };
 </script>
