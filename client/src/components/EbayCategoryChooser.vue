16c76e0170e5f14f5042e606da2e4b2db314420f
Ed Salisbury <ed.salisbury@gmail.com>
2022-06-11 14:19:48 -0600

    Refactored EbayCategoryChooser to be more concise

--- client/src/components/EbayCategoryChooser.vue
+++ client/src/components/EbayCategoryChooser.vue
@@ -1,57 +1,14 @@
 <template>
   <b-container>
     <b-row>
-      <b-col>
+      <b-col v-for="(level, index) in levels" :key="'ebayCategoryCol-' + index">
         <b-form-select
-          id="ebayCategory-0"
-          v-model="levels[0]"
-          :options="getCategories(0)"
-          @change="resetCategories(0)"
-        />
-      </b-col>
-
-      <b-col v-if="levels[0] > 0 && getCategories(levels[0]).length > 0">
-        <b-form-select
-          id="ebayCategory-1"
-          v-model="levels[1]"
-          :options="getCategories(levels[0])"
-          @change="resetCategories(1)"
-        />
-      </b-col>
-
-      <b-col v-if="levels[1] > 0 && getCategories(levels[1]).length > 0">
-        <b-form-select
-          id="ebayCategory-2"
-          v-model="levels[2]"
-          :options="getCategories(levels[1])"
-          @change="resetCategories(2)"
-        />
-      </b-col>
-
-      <b-col v-if="levels[2] > 0 && getCategories(levels[2]).length > 0">
-        <b-form-select
-          id="ebayCategory-3"
-          v-model="levels[3]"
-          :options="getCategories(levels[2])"
-          @change="resetCategories(3)"
-        />
-      </b-col>
-
-      <b-col v-if="levels[3] > 0 && getCategories(levels[3]).length > 0">
-        <b-form-select
-          id="ebayCategory-4"
-          v-model="levels[4]"
-          :options="getCategories(levels[3])"
-          @change="resetCategories(4)"
-        />
-      </b-col>
-
-      <b-col v-if="levels[4] > 0 && getCategories(levels[4]).length > 0">
-        <b-form-select
-          id="ebayCategory-5"
-          v-model="levels[5]"
-          :options="getCategories(levels[4])"
-          @change="resetCategories(5)"
+          v-if="index === 0 || (levels[index - 1] > 0 && getCategories(levels[index - 1]).length > 0)"
+          :key="'ebayCategory-' + index"
+          :id="'ebayCategory-' + index"
+          v-model="levels[index]"
+          :options="getCategories(levels[index - 1])"
+          @change="resetCategories(index)"
         />
       </b-col>
     </b-row>
@@ -67,11 +24,12 @@ export default {
   data() {
     return {
       ebayCategories: [],
-      maxLevels: 6,
-      levels: [0, 0, 0, 0, 0, 0],
+      maxLevels: 10,
+      levels: [],
     };
   },
   async created() {
+    this.resetCategories(0);
     const token = this.$cookie.get("token");
     this.ebayCategories = await api.getEbayCategories(token);
   },
