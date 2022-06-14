6fbf00d783e8a60b667c3394ccb1e7aac247e19a
Ed Salisbury <ed.salisbury@gmail.com>
2022-06-14 15:49:18 -0600

    Added Add Item button to top of items list

--- client/src/components/ItemList.vue
+++ client/src/components/ItemList.vue
@@ -2,64 +2,34 @@
   <b-card>
     <b-card-title>Items</b-card-title>
     <b-card-body>
-      <b-table
-        hover
-        :items="items"
-        :fields="fields"
-      >
+      <router-link to="/addItem"><b-button variant="primary">Add Item</b-button></router-link>
+      <b-table hover :items="items" :fields="fields">
         <template #cell(itemLink)="data">
-          <router-link
-            :to="'/viewItem/' + data.item.id"
-          >
+          <router-link :to="'/viewItem/' + data.item.id">
             {{ data.item.title }}
           </router-link>
         </template>
 
         <template #cell(ebayListingId)="data">
-          <a
-            v-bind:href="
-              'https://www.ebay.com/itm/' +
-              data.item.ebayListingId
-            "
-            target="_blank"
-          >
+          <a v-bind:href="'https://www.ebay.com/itm/' + data.item.ebayListingId" target="_blank">
             {{ data.item.ebayListingId }}
           </a>
         </template>
 
         <template #cell(actions)="data">
-          <router-link
-            :to="'/editItem/' + data.item.id"
-          >
-            <b-button
-              class="p-1 m-1"
-              variant="primary"
-            >
+          <router-link :to="'/editItem/' + data.item.id">
+            <b-button class="p-1 m-1" variant="primary">
               <b-icon-pencil-fill />
             </b-button>
           </router-link>
-          <b-button
-            class="p-1 m-1"
-            variant="primary"
-            @click="listItem(data.item.id)"
-          >
-            eBay</b-button
-          >
+          <b-button class="p-1 m-1" variant="primary" @click="listItem(data.item.id)"> eBay</b-button>
 
-          <b-button
-            class="p-1 m-1"
-            variant="danger"
-            @click="deleteItem(data.item.id)"
-          >
+          <b-button class="p-1 m-1" variant="danger" @click="deleteItem(data.item.id)">
             <b-icon-trash-fill />
           </b-button>
         </template>
       </b-table>
-      <router-link to="/addItem"
-        ><b-button variant="primary"
-          >Add Item</b-button
-        ></router-link
-      >
+      <router-link to="/addItem"><b-button variant="primary">Add Item</b-button></router-link>
     </b-card-body>
   </b-card>
 </template>
@@ -89,16 +59,13 @@ export default {
   methods: {
     async listItem(id) {
       const token = this.$cookie.get("token");
-      const url =
-        process.env.VUE_APP_API_BASE_URL +
-        "/api/v1/ebayListings";
+      const url = process.env.VUE_APP_API_BASE_URL + "/api/v1/ebayListings";
       const response = await fetch(url, {
         method: "POST",
 
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            "Bearer " + this.$cookie.get("token"),
+          Authorization: "Bearer " + this.$cookie.get("token"),
         },
         body: JSON.stringify({
           itemId: id,
@@ -113,21 +80,15 @@ export default {
     },
 
     async deleteItem(id) {
-      const url =
-        process.env.VUE_APP_API_BASE_URL +
-        "/api/v1/items/" +
-        id;
+      const url = process.env.VUE_APP_API_BASE_URL + "/api/v1/items/" + id;
       const response = await fetch(url, {
         method: "DELETE",
         headers: {
-          Authorization:
-            "Bearer " + this.$cookie.get("token"),
+          Authorization: "Bearer " + this.$cookie.get("token"),
         },
       });
       if (response.status == 204) {
-        this.items = this.items.filter(
-          (item) => item.id !== id,
-        );
+        this.items = this.items.filter((item) => item.id !== id);
       } else {
         console.error(response);
       }
